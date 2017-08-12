372fb83ea19b8097a271ff1f534417f77b67d580
Josh Meier <josh@jm-debian.home>
2017-08-12 00:39:20 -0400

    All the is* functions added, as well as atoi.  Libft.h updated to reflect this.  I should update the makefile soon though

--- libft.h
+++ libft.h
@@ -12,13 +12,20 @@ void	*ft_memset(void *s, int c, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 
-
 char	*ft_strdup(const char *src);
 size_t	ft_strlen(const char *s);
 char	*ft_strncpy(char *dst, const char *src, size_t n);
 char	*ft_strcpy(char *dst, const char *src);
 char	*ft_strcat(char *dst, const char *src);
 char	*ft_strncat(char *dst, const char *src, size_t n);
+
+int		ft_atoi(const char *str);
+
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isprint(int c);
+int		ft_isascii(int c);
 
 void	ft_putchar(char c);
 void	ft_putstr(char *str);
