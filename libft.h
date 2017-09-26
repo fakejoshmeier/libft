31e7a4184b9b9d034193c22c8766e406edb1a819
fakejoshmeier <jmeier@student.42.us.org>
2017-09-25 17:35:43 -0700

    Update libft.h

--- libft.h
+++ libft.h
@@ -20,7 +20,7 @@ int		ft_atoi(const char *str);
 void	ft_bzero(void *s, size_t n);
 int		ft_isalnum(int c);
 int		ft_isalpha(int c);
-int		ft_isalpha(int c);
+int		ft_isascii(int c);
 int		ft_isdigit(int c);
 int		ft_isprint(int c);
 void	*ft_memalloc(size_t size);
