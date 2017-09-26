ed2feb258923eef3d1b07b66d5c717c226bad9e0
Joshua Meier <jmeier@e1z2r9p46.42.us.org>
2017-09-25 20:20:17 -0700

    Last additional files before bonus

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,31 +6,52 @@
 /*   By: jmeier <jmeier@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/24 23:31:34 by jmeier            #+#    #+#             */
-/*   Updated: 2017/09/24 23:33:49 by jmeier           ###   ########.fr       */
+/*   Updated: 2017/09/25 16:01:26 by jmeier           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strtrim(char const *s)
+static size_t	blank_space_b(char *s)
+{
+	size_t a;
+
+	a = 0;
+	while ((s[a]) && (s[a] == ' ' || s[a] == '\n' || s[a] == '\t'))
+		a++;
+	return (a);
+}
+
+static size_t	blank_space_e(char *s)
+{
+	size_t a;
+
+	a = ft_strlen(s) - 1;
+	while (s[a] == ' ' || s[a] == '\n' || s[a] == '\t')
+		a--;
+	return (a);
+}
+
+char			*ft_strtrim(char const *s)
 {
 	size_t	a;
 	size_t	b;
-	size_t	c;
-	size_t	d;
+	size_t	i;
 	char	*str;
 
-	a = 0;
-	while (s[a++] && !c)
-		if (s[a] != ' ' || s[a] != '\n' || s[a] != '\t')
-			c = a;
-	b = ft_strlen(s);
-	while (s[b--] && !d)
-		if (s[b] != ' ' || s[b] != '\n' || s[b] != '\t')
-			d = b;
-	str = ft_strnew((ft_strlen(s) - c) - (ft_strlen(s) - d));
-	a = 0;
-	while (c < d)
-		str[a++] = s[c++];
+	if (!s)
+		return (NULL);
+	a = blank_space_b((char *)s);
+	b = blank_space_e((char *)s);
+	if (a == 0 && b == ft_strlen(s) - 1)
+		return ((char *)s);
+	if (a == ft_strlen(s) || b == 0)
+		return (ft_strnew(0));
+	str = ft_strnew((b - a) + 1);
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (a <= b)
+		str[i++] = s[a++];
 	return (str);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: jmeier <jmeier@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/18 11:34:56 by jmeier            #+#    #+#             */
-/*   Updated: 2017/09/24 23:27:45 by jmeier           ###   ########.fr       */
+/*   Updated: 2017/09/25 20:16:21 by jmeier           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,11 @@ int		ft_atoi(const char *str);
 void	ft_bzero(void *s, size_t n);
 int		ft_isalnum(int c);
 int		ft_isalpha(int c);
-int		ft_isascii(int c);
 int		ft_isdigit(int c);
 int		ft_isprint(int c);
+int		ft_isascii(int c);
+char	*ft_itoa(int n);
+char	*ft_itoa_base(int n, int base);
 void	*ft_memalloc(size_t size);
 void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
@@ -40,6 +42,7 @@ void	ft_putnbr_fd(int n, int fd);
 void	ft_putstr(char *s);
 void	ft_putstr_fd(char const *s, int fd);
 char	*ft_strcat(char *dst, const char *src);
+char	*ft_strchr(const char *s, int c);
 void	ft_strclr(char *s);
 int		ft_strcmp(const char *s1, const char *s2);
 char	*ft_strcpy(char *dst, const char *src);
@@ -60,6 +63,7 @@ int		ft_strnequ(char const *s1, char const *s2, size_t n);
 char	*ft_strnew(size_t size);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
 char	*ft_strrchr(const char *s, int c);
+char	**ft_strsplit(char const *s, char c);
 char	*ft_strstr(const char *big, const char *little);
 char	*ft_strsub(char const *s, unsigned int start, size_t len);
 char	*ft_strtrim(char const *s);
