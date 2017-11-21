640c8364940bc70e0d60c5ee9a43e7cd761a3f52
Joshua Meier <jmeier@e1z3r6p1.42.us.org>
2017-11-21 11:44:55 -0800

    putnbr_base done

--- ft_putnbr_base.c
+++ ft_putnbr_base.c
@@ -1,35 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*   ft_putnbr_base.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jmeier <jmeier@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/20 12:52:12 by jmeier            #+#    #+#             */
-/*   Updated: 2017/11/21 11:26:48 by jmeier           ###   ########.fr       */
+/*   Updated: 2017/11/21 11:25:15 by jmeier           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void		ft_putnbr_fd(int n, int fd)
+void	ft_putnbr_base(int n, int base)
 {
-	if (n < 0)
-	{
-		ft_putchar_fd('-', fd);
-		n = -n;
-	}
-	if (n == -2147483648)
-	{
-		ft_putchar_fd('2', fd);
-		n %= 1000000000;
-		n = -n;
-	}
-	if (n >= 10)
-	{
-		ft_putnbr_fd(n / 10, fd);
-		ft_putnbr_fd(n % 10, fd);
-	}
-	else
-		ft_putchar_fd(n + '0', fd);
+	ft_putnbr_base_fd(n, base, 1);
 }
--- ft_putnbr_base_fd.c
+++ ft_putnbr_base_fd.c
@@ -1,35 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*   ft_putnbr_base_fd.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jmeier <jmeier@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/20 12:52:12 by jmeier            #+#    #+#             */
-/*   Updated: 2017/11/21 11:26:48 by jmeier           ###   ########.fr       */
+/*   Updated: 2017/11/21 11:43:19 by jmeier           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void		ft_putnbr_fd(int n, int fd)
+void	ft_putnbr_base_fd(int n, int base, int fd)
 {
+	int		p;
+	char	*c;
+
+	p = 1;
+	c = "0123456789ABCDEFHIJKLMNOPQRSTUVWXYZ";
+	while ((n / p) >= base || (n / p) <= -base)
+		p *= base;
 	if (n < 0)
-	{
 		ft_putchar_fd('-', fd);
-		n = -n;
-	}
-	if (n == -2147483648)
+	base == 2 ? ft_putstr_fd("0b", fd) : 0;
+	base == 8 ? ft_putstr_fd("0o", fd) : 0;
+	base == 16 ? ft_putstr_fd("0x", fd) : 0;
+	while (p > 0)
 	{
-		ft_putchar_fd('2', fd);
-		n %= 1000000000;
-		n = -n;
+		ft_putchar_fd(c[(n / p) % base], fd);
+		p /= base;
 	}
-	if (n >= 10)
-	{
-		ft_putnbr_fd(n / 10, fd);
-		ft_putnbr_fd(n % 10, fd);
-	}
-	else
-		ft_putchar_fd(n + '0', fd);
 }
